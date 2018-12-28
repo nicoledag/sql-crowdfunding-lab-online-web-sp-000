e1db3173aca9333b596a1f9400678815031fcc70
Nicole D <nnardella@gmail.com>
2018-12-28 02:25:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT projects.title, SUM(pledges.amount)
+  "SELECT user_name.age, SUM(pledges.amount)
    FROM projects
    JOIN pledges ON projects.id = pledges.project_id
    GROUP BY (projects.title)
