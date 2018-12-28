dda4f4aa0753ef0bd7a459433928f7798bc1ed4a
Nicole D <nnardella@gmail.com>
2018-12-28 03:11:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,11 +39,11 @@ ORDER BY SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, SUM(pledges.amount)
+"SELECT projects.category, pledges.amount
 FROM projects
 JOIN pledges
 ON pledges.project_id = projects.id;
-
+WHERE projects.category = 'Music';
 
 "
 end
